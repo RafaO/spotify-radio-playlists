3db1104bf265e53283f718ebd29f5a10d310bc7b
RafaO <rafao1989@hotmail.com>
2022-11-20 18:47:16 +0100

    add method to save songs to playlist

--- canal-fiesta-crawler/src/spotifyCient.ts
+++ canal-fiesta-crawler/src/spotifyCient.ts
@@ -1,13 +1,14 @@
 export class SpotifyClient {
 
     private _authToken: string;
-    private _url: string = "https://api.spotify.com/v1/search?limit=1&type=track";
  
     constructor(authToken: string) {
         this._authToken = authToken;
     }
 
     async searchSongs(searchStrings: String[]) {        
+
+        const url = "https://api.spotify.com/v1/search?limit=1&type=track"
 
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
@@ -19,11 +20,36 @@ export class SpotifyClient {
             redirect: 'follow'
         };
 
-        searchStrings.forEach((element: String) => {
-            fetch(`${this._url}&q=${encodeURIComponent(element.toString())}`, requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
+        const strings = new Array();
+
+        for (const element of searchStrings.slice(0,20)) {
+            await fetch(`${url}&q=${encodeURIComponent(element.toString())}`, requestOptions)
+                .then(response => response.json())
+                .then(result => strings.push(result['tracks']['items'][0].uri))
                 .catch(error => console.log('error', error));
-        });
+        }
+        this.addSongsToPlaylist(strings.join(','));
+    }
+
+    async addSongsToPlaylist(songsIds: String) {
+        const canalFiestaPlaylistUri = "0zAkuuNspqWamgwJsIGjIk";
+        
+        const url = `https://api.spotify.com/v1/playlists/${canalFiestaPlaylistUri}/tracks?uris=${songsIds}`
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Bearer ${this._authToken}`);
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            redirect: 'follow',
+            body: JSON.stringify({
+                "range_start": 0,
+                "range_length": songsIds.length
+              })
+        };
+
+        fetch(`${url}`, requestOptions).catch(error => console.log('error', error));
     }
 }
