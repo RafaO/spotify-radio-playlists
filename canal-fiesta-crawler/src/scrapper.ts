4074620c14a1d565f4e9a266f0853ab12103110e
RafaO <rafao1989@hotmail.com>
2024-04-07 13:41:20 +0200

    fix html scrapping (again)

--- canal-fiesta-crawler/src/scrapper.ts
+++ canal-fiesta-crawler/src/scrapper.ts
@@ -6,7 +6,7 @@ export class CanalFiestaScraper {
         const $ = load(response);
 
         const strings = new Array();
-        $("div.textoNoticia.top20 > table > tbody > tr > td > table > tbody").children().each((_index, listRow) => {
+        $("div.textoNoticia.top20 > table > tbody").children().each((_index, listRow) => {
             strings.push($(listRow).children().slice(-2).map((_index, cell) => {
                 return $(cell).text().trim();
             }).toArray().reverse().join(', '));
