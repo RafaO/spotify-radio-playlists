45779fb3bd8d4c4d7cf1837c22cad5eeb87d57a7
RafaO <rafao1989@hotmail.com>
2024-10-20 21:49:19 +0200

    adjust main function

--- canal-fiesta-crawler/src/index.ts
+++ canal-fiesta-crawler/src/index.ts
@@ -1,3 +1,4 @@
+import { Handler } from 'aws-lambda';
 import { CanalFiestaScraper } from "./scrapper";
 import { SpotifyClient } from "./spotifyCient";
 import { SpotifyAuth } from "./spotifyAuth";
@@ -6,67 +7,55 @@ import { initLogger } from "./logging";
 import { SearchRepository } from "./searchRepository";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GenAIHelper } from "./genAIHelper";
+import { AWSSecretsManager } from './AWSSecretsManager';
 
-/**
- * Welcome to Cloudflare Workers! This is your first scheduled worker.
- *
- * - Run `wrangler dev --local` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
- * - Go back to the console to see what your worker has logged
- * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
- * - Run `wrangler publish --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
- */
+const url = 'https://www.canalsur.es/radio/programas/cuenta-atras/noticia/1305888.html';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	SONG_IDS: KVNamespace;
-}
+export const handler: Handler = async (event, context) => {
+	initLogger(process.env.SENTRY_DSN || '', context);
+	Logger.debug("worker starting");
 
-export default {
-	async scheduled(
-		controller: ScheduledController,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<void> {
+	const secretsManager = new AWSSecretsManager();
+	const spotifySecrets = await secretsManager.getSecret();
 
-		initLogger(env.SENTRY_DSN, ctx);
-		Logger.debug("worker starting");
+	if (!spotifySecrets) {
+		throw new Error('error retrieving secrets');
+	}
 
-		const spotifyAuth = new SpotifyAuth(env.REFRESH_TOKEN, env.CLIENT_ID, env.CLIENT_SECRET);
-		const code = await spotifyAuth.getAccessToken();
-		const genAIApiKey: string = env.GEN_AI_API_KEY;
-		const genAIScrapper: GenAIHelper = new GenAIHelper(new GoogleGenerativeAI(genAIApiKey));
+	Logger.debug("environment variables retrieved");
+	const spotifyAuth = new SpotifyAuth(spotifySecrets.refresh_token, spotifySecrets.client_id, spotifySecrets.client_secret);
+	const code = await spotifyAuth.getAccessToken();
 
-		if (code != null) {
-			Logger.debug("access token received");
+	const genAIApiKey: string = "env.GEN_AI_API_KEY";
+	const genAIScrapper: GenAIHelper = new GenAIHelper(new GoogleGenerativeAI(genAIApiKey));
 
-			const scraper = new CanalFiestaScraper(genAIScrapper);
-			const spotifyApi = new SpotifyClient(code);
-			const searchRepository = new SearchRepository(spotifyApi, env.SONG_IDS);
+	if (code != null) {
+		Logger.debug("access token received");
 
-			const searchStrings = await scraper.scrapeList("https://www.canalsur.es/radio/programas/cuenta-atras/noticia/1305888.html");
-			if (searchStrings.length != 50 ) {
-				Logger.error("something went wrong scrapping the list");
-				return;
-			}
-			Logger.debug("search strings received");
-			Logger.debug(searchStrings);
+		const scraper = new CanalFiestaScraper(genAIScrapper);
+		const spotifyApi = new SpotifyClient(code);
+		const searchRepository = new SearchRepository(spotifyApi, "env.SONG_IDS");
 
-			await searchRepository.cleanUpKV(searchStrings);
-			Logger.debug("kv cleaned up");
-
-			let songIds = await searchRepository.getSongIds(searchStrings);
-
-			Logger.debug("song ids received");
-			Logger.debug(songIds);
-
-			await spotifyApi.addSongsToPlaylist(songIds.join(','));
-
-			Logger.debug("songs added to playlist - finishing");
-		} else {
-			Logger.error("access code is null");
+		const searchStrings = await scraper.scrapeList("https://www.canalsur.es/radio/programas/cuenta-atras/noticia/1305888.html");
+		if (searchStrings.length != 50 ) {
+			Logger.error("something went wrong scrapping the list");
+			return;
 		}
-	},
+		Logger.debug("search strings received");
+		Logger.debug(searchStrings);
+
+		// await searchRepository.cleanUpKV(searchStrings);
+		// Logger.debug("kv cleaned up");
+
+		let songIds = await searchRepository.getSongIds(searchStrings);
+
+		Logger.debug("song ids received");
+		Logger.debug(songIds);
+
+		await spotifyApi.addSongsToPlaylist(songIds.join(','));
+
+		Logger.debug("songs added to playlist - finishing");
+	} else {
+		Logger.error("access code is null");
+	}
 };
